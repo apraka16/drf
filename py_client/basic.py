2e9275c041126f4cc0dfe7fd31d237b255d56660
Abhinav Prakash <abhinav.prakash1@gmail.com>
2023-02-05 16:00:13 +0530

    Products module

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,8 +1,12 @@
 import requests
 
-endpoint = 'https://httpbin.org/anything'
+"""A public API endpoint to play with"""
+# endpoint = 'https://httpbin.org/anything'
+
+endpoint = 'http://localhost:8000/api/'
 
 # Client is requesting the endpoint for data
-get_response = requests.get(endpoint, json={'query': 'hello world'})
+get_response = requests.get(endpoint, params={'abc': 123}, json={'query':'hello world'})
 # print(get_response.text)
+# print(get_response.status_code)
 print(get_response.json())
