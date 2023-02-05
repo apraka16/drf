7e6022c6fabba2f21a2f4ce3ac34b345a88c5355
Abhinav Prakash <abhinav.prakash1@gmail.com>
2023-02-05 21:30:55 +0530

    Create, List done

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from django.shortcuts import get_object_or_404
 
 
 from .models import Product
@@ -25,8 +29,34 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
 product_detail_view = ProductDetailAPIView.as_view()
 
 """No need to use. This is adjusted into ProductListCreateView"""
-class ProductListAPIView(generics.ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+# class ProductListAPIView(generics.ListAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductSerializer
 
-product_list_view = ProductDetailAPIView.as_view()
+# product_list_view = ProductDetailAPIView.as_view()
+
+"""Functional way to do the same thing - NOT being used"""
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method
+
+    if method == 'GET':
+        if pk is not None:
+            # details view
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+        # list view
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+    if method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+        return Response({"invalid":"not good data"}, status=400)
---
+++ py_client/not_found.py
@@ -0,0 +1,10 @@
+import requests
+
+"""A public API endpoint to play with"""
+# endpoint = 'https://httpbin.org/anything'
+
+endpoint = 'http://localhost:8000/api/products/1439732457/'
+
+# Client is requesting the endpoint for data
+get_response = requests.get(endpoint)
+print(get_response.json())
