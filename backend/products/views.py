b9c7297bfd8e56863c07745b5a8786c1a10200fd
Abhinav Prakash <abhinav.prakash1@gmail.com>
2023-02-12 20:34:26 +0530

    Mixin done

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, mixins
+from rest_framework import generics, mixins, permissions, authentication
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
@@ -8,10 +8,13 @@ from django.shortcuts import get_object_or_404
 from .models import Product
 from .serializers import ProductSerializer
 
+
 # To manage prouduct list and product create
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.DjangoModelPermissions]
 
     def perform_create(self, serializer):
         title = serializer.validated_data.get('title')
@@ -19,6 +22,7 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         if content is None:
             content = title
         serializer.save(content=content)
+
 
 product_list_create_view = ProductListCreateAPIView.as_view()
 
@@ -28,6 +32,7 @@ class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # lookup_field = 'pk'
+
 
 product_detail_view = ProductDetailAPIView.as_view()
 
@@ -46,6 +51,7 @@ class ProductUpdateAPIView(generics.UpdateAPIView):
 
 product_update_view = ProductUpdateAPIView.as_view()
 
+
 # To delete product
 class ProductDestroyAPIView(generics.DestroyAPIView):
     queryset = Product.objects.all()
@@ -54,6 +60,7 @@ class ProductDestroyAPIView(generics.DestroyAPIView):
 
     def perform_destroy(self, instance):
         super().perform_destroy(instance)
+
 
 product_destroy_view = ProductDestroyAPIView.as_view()
 
@@ -64,9 +71,10 @@ product_destroy_view = ProductDestroyAPIView.as_view()
 
 # product_list_view = ProductDetailAPIView.as_view()
 
-class ProductMixinView(mixins.CreateModelMixin, 
-    mixins.ListModelMixin, 
-    mixins.RetrieveModelMixin, 
+
+class ProductMixinView(mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
     generics.GenericAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
@@ -77,7 +85,7 @@ class ProductMixinView(mixins.CreateModelMixin,
         if pk is not None:
             return self.retrieve(request, *args, **kwargs)
         return self.list(request, *args, **kwargs)
-    
+
     def post(self, request, *args, **kwargs):
         return self.create(request, *args, **kwargs)
 
@@ -87,9 +95,10 @@ class ProductMixinView(mixins.CreateModelMixin,
         if content is None:
             content = 'This is a single view doing stuff'
         serializer.save(content=content)
- 
+
+
 product_mixin_view = ProductMixinView.as_view()
- 
+
 """Functional way to do the same thing - NOT being used"""
 @api_view(['GET', 'POST'])
 def product_alt_view(request, pk=None, *args, **kwargs):
